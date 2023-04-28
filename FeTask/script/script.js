b0e848f43e84c10fb23017a3c3881f9fc61d951a
fiestadi <124197643+fiestadi@users.noreply.github.com>
2023-04-28 23:16:19 +0200

    FEHW3

---
+++ FeTask/script/script.js
@@ -0,0 +1,91 @@
+// // Task1
+
+// let d = 7; 
+// console.log(d);
+// console.log(d*d); 
+// console.log(d*d*d);
+
+
+// // Task2
+
+// let num = Number(prompt("Enter your number:")); 
+
+// if (num> 0) { 
+//   num *= 2; 
+//   console.log("New number: " + num); 
+// } else {
+//   console.log("Number is not positive"); }
+
+  //Task3
+
+//   let num1 = Number(prompt("Enter first integer:")); 
+//   let num2 = Number(prompt("Enter second integer:")); 
+  
+//   if (num1 > num2) { 
+//     console.log("Addition: " + (num1 + num2)); 
+//   } else if (num1 < num2) { 
+//     console.log("Multiplication: " + (num1 * num2)); 
+//   } else { 
+//     console.log("All numbers are equal"); 
+//   }
+
+  //Task4
+
+// let num = Number(prompt("Enter integer:")); 
+// if (num < 0) { 
+//   console.log("Square numbers: " + (num * num)); 
+// } else { 
+//   console.log(num);}
+
+//Task5
+
+// let studentsCount = Number(prompt("Enter the number of students in the class:")); 
+// let chairsCount = Number(prompt("Enter the number of chairs in the class:")); 
+// if (studentsCount <= chairsCount) { 
+//   console.log("Enough chairs."); 
+// } else { 
+//   console.log("Not enough chairs."); }
+
+//Task6
+// console.log("Start");
+
+// for (let i = 1; i <= 9; i++) { 
+//   console.log(i); }
+
+// console.log("Finish");
+
+//Task7
+
+// for (let i = 10; i <= 99; i++) { 
+//     if (i % 5 == 0) { 
+//       console.log(i); 
+//     }
+//   }
+
+//Task8
+
+// const arr = [5, 10, 15, 20, 25, 30];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] >= 15) {
+//     console.log(arr[i]);
+//   }
+// }
+
+//Task9
+
+// for (let i = 1; i <= 15; i++) {
+//     if (i % 2 !== 0) {
+//       console.log(i);
+//     }
+//   }
+
+//Task10
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (i % 3 === 0) {
+//     console.log(arr[i]);
+//   }
+// }
